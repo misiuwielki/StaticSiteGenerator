23fc915337d263e2998beccd278ecb25e134deb1
misiuwielki <misiu.m10@gmail.com>
2026-03-03 14:19:08 +0100

    added more pages and recursive page generation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from static_copy import copy_to_public
-from page_generator import generate_page
+from page_generator import generate_page, generate_page_recursive
 
 def main():
     dir_path_public = "public"
@@ -9,6 +9,6 @@ def main():
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     copy_to_public(dir_path_static, dir_path_public)
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page_recursive("content", "template.html", "public")
     
 main()
--- src/page_generator.py
+++ src/page_generator.py
@@ -1,6 +1,7 @@
 from markdown_to_text import *
 import re
 import os
+from pathlib import Path
 from markdown_to_html import markdown_to_html
 
 def extract_title(markdown):
@@ -22,3 +23,26 @@ def generate_page(source_path, template_path, dest_path):
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(template)
+
+def generate_page_recursive(source_dir_path, template_path, dest_dir_path):
+    print(f"Generating pages from {source_dir_path} to {dest_dir_path} using {template_path}")
+    source_list = os.listdir(source_dir_path)
+    for filename in source_list:
+        file_path = os.path.join(source_dir_path, filename)
+        if os.path.isfile(file_path) and filename.endswith(".md"):
+            dest_path = Path(os.path.join(dest_dir_path, filename)).with_suffix(".html")
+            with open(file_path, "r", encoding="utf-8") as f:
+                source = f.read()
+            with open(template_path, "r", encoding="utf-8") as f:
+                template = f.read()
+            html = markdown_to_html(source)
+            title = extract_title(source)
+            template = template.replace("{{ Title }}", title)
+            template = template.replace("{{ Content }}", html)
+            os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+            print(f"Generating page from {file_path} to {dest_path}")
+            with open(dest_path, "w", encoding="utf-8") as f:
+                f.write(template)
+        else: 
+            dest_path = os.path.join(dest_dir_path, filename)
+            generate_page_recursive (file_path, template_path, dest_path)
